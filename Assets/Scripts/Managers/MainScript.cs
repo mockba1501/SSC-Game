82f7ceac2de41508b93ec320bc9f3c4b66b19a5b
Shireen <shireenpeerbuksh2011@gmail.com>
2022-12-13 10:04:26 +0200

    change

--- Assets/Scripts/Managers/MainScript.cs
+++ Assets/Scripts/Managers/MainScript.cs
@@ -6,19 +6,46 @@ using UnityEngine.SceneManagement;
 public class MainScript : MonoBehaviour
 {
     public string title;
+    private string PrevScene;
+    //private string scene1;
+   // private string[] scene1 = new string[2];
+    private List<string> scene1 = new List<string>();  //running history of scenes
+ 
+    Scene scene;  
+  public  void Start()
+    {
+
+           scene = SceneManager.GetActiveScene();
+           PrevScene = PlayerPrefs.GetString("SceneNumber");
+           PlayerPrefs.SetString("SceneNumber", SceneManager.GetActiveScene().name);
+        //scene1.Add(scene.name);
+        Debug.Log("Active Scene name is: " + scene.name + "\nActive Scene index: " + scene.buildIndex);
+    }
     
     // Start is called before the first frame update
    public void ChangeSecneDynamic(string title)
-    {
-         SceneManager.LoadScene(title);    
+    {  
+       //scene1.Add(title);
+      
+       SceneManager.LoadScene(title);    
+       
     
     }
 
-    public void ChangeScene(int sceneIndex)
-    {
-        SceneManager.LoadScene(sceneIndex);
+    public void ChangeScene()
+    { 
+          Debug.Log("Active Scene name is: " + scene.name + "\nActive Scene index: " + scene.buildIndex);
+        
+       Debug.Log(" name is: " + title);
+          //Debug.Log(" name is: " + title2);
+       Debug.Log("Active Scene name is: " + scene.name + "\nActive Scene index: " + scene.buildIndex +"::" +scene1.Count + "::"+PrevScene);
+       
+      SceneManager.LoadScene(PrevScene);  
+    
+       
     }
 
+   
 
-    
+   
 }
